b236d179b145c47a02c01d9022aa5290b14583bc
L_Dem_L <ostapvasilenko55@gmail.com>
2023-07-28 19:59:14 +0300

    end

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -1,2 +1,11 @@
+# def pib(n, s, f):
+#     pib = s + ' '+ n[0] + ' ' + f[0]
+#     return pib
+
+
 if __name__ == '__main__':
-    print('yep')
+    # arr = []
+    # for i in range(int(input('--> ')),int(input('--> '))+1):
+    #     if i % 3 == 0 and i % 5 == 0:
+    #         arr.append(i)
+    # print(arr)
